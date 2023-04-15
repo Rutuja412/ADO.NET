0e24cdb4259594ffc885623c0a9fac1ed7132ee1
Rutuja412 <rutujakadam4545@gmail.com>
2023-04-15 22:24:53 +0530

    [Rutuja]:Welcome

---
+++ ADO.NET-PracticeProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADO.NET_PracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ADO.NET Practice Problem");
+        }
+    }
+}
